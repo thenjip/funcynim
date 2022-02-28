da03cbb73e1435462b970506d649fbecff9851a3
thenjip <39303840+thenjip@users.noreply.github.com>
2022-02-28 16:47:04 +0100

    Add nimcall proc composition

--- src/funcynim/chain.nim
+++ src/funcynim/chain.nim
@@ -8,9 +8,25 @@ import std/[sugar]
 
 
 
+func chain*[A; B; C](
+  first: proc (_: A): B {.nimcall.};
+  second: proc (_: B): C {.nimcall.}
+): proc (_: A): C {.nimcall.} =
+  ## Since `0.3.0`.
+  (a: A) {.nimcall.} => a.first().second()
+
+
+func chain*[A; B](
+  first: proc (): A {.nimcall.};
+  second: proc (_: A): B {.nimcall.}
+): proc (): B {.nimcall.} =
+  ## Since `0.3.0`.
+  () {.nimcall.} => first().second()
+
+
+
 func chain*[A; B; C](f: A -> B; g: B -> C): A -> C =
   (a: A) => a.f().g()
-
 
 
 func chain*[A; B](f: () -> A; g: A -> B): () -> B =
--- tests/chain/test.nim
+++ tests/chain/test.nim
@@ -1,13 +1,28 @@
 when isMainModule:
   import pkg/funcynim/[chain]
 
-  import std/[unittest]
+  import std/[sugar, unittest]
 
 
 
   proc main() =
     suite "chain":
-      discard
+      test "Chaining nimcall procs together should give a nimcall proc.":
+        proc doTest[A; B; C](
+          first: proc (_: A): B {.nimcall.};
+          second: proc (_: B): C {.nimcall.}
+        ) =
+          template actual(): typedesc[proc] =
+            first.chain(second).typeof()
+
+          template expected(): typedesc[proc (_: A): C {.nimcall.}] =
+            typeof(proc (_: A): C {.nimcall.})
+
+          check:
+            actual() is expected()
+
+        doTest((s: string) => s, s => s.len())
+
 
 
 
