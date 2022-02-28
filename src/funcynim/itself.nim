fc361e1cc56122dde4bf7b26aaddca9a5c17196a
thenjip <39303840+thenjip@users.noreply.github.com>
2022-02-28 21:39:27 +0100

    Add the identity proc

---
+++ src/funcynim/itself.nim
@@ -0,0 +1,13 @@
+##[
+  Since `0.3.0`.
+]##
+
+
+
+proc itself*[T](self: T): T =
+  ##[
+    The identity function.
+
+    Since `0.3.0`.
+  ]##
+  self
---
+++ tests/itself/test.nim
@@ -0,0 +1,14 @@
+when isMainModule:
+  import pkg/funcynim/[itself]
+
+  import std/[unittest]
+
+
+
+  proc main() =
+    suite "itself":
+      discard
+
+
+
+  main()
