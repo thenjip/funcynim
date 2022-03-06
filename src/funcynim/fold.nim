85c78d7e6c2d145ec6dabacc3f141d0a858642f7
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-06 18:06:19 +0100

    Add "fold" module

---
+++ src/funcynim/fold.nim
@@ -0,0 +1,21 @@
+##[
+  Since `0.3.0`.
+]##
+
+
+
+import run, unit
+
+import std/[sugar]
+
+
+
+proc fold*[T](self: bool; then, `else`: Unit -> T): T =
+  ##[
+    The `if else` expression reified as a `proc`.
+
+    Since `0.3.0`.
+  ]##
+  let path = if self: then else: `else`
+
+  path.run()
