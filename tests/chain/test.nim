c7f22aea1bf6a45b301ad34ee1d2fc73fe696e9f
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-07 18:25:23 +0100

    Add a test for the associativity of "chain"

--- tests/chain/test.nim
+++ tests/chain/test.nim
@@ -1,13 +1,28 @@
 when isMainModule:
   import pkg/funcynim/[chain]
 
-  import std/[unittest]
+  import std/[sugar, unittest]
 
 
 
   proc main() =
     suite "chain":
-      discard
+      test "Chaining procs should be associative.":
+        proc doTest[A; B; C; D](
+          first: A -> B;
+          second: B -> C;
+          third: C -> D;
+          input: A
+        ) =
+          let
+            leftHand = first.chain(second.chain(third))
+            rightHand = first.chain(second).chain(third)
+
+          check:
+            leftHand(input) == rightHand(input)
+
+
+        doTest((i: int) => i + 1, i => $i, s => s.len(), 0)
 
 
 
