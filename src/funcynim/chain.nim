55013d14a8c544a11ffcc43086d656436002b42a
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-07 18:42:16 +0100

    Add `compose` proc to the `chain` module (#62)
    * Mark "chain(f: () -> A, ...)" as deprecated
    
    Use "into" proc in implementation.
    
    * Add "compose" proc
    
    * Add a test for the associativity of "chain"

--- src/funcynim/chain.nim
+++ src/funcynim/chain.nim
@@ -4,14 +4,25 @@
 
 
 
+import into
+
 import std/[sugar]
 
 
 
 func chain*[A; B; C](f: A -> B; g: B -> C): A -> C =
-  (a: A) => a.f().g()
+  (a: A) => f(a).into(g)
+
+
+func chain*[A; B](f: () -> A; g: A -> B): () -> B {.
+  deprecated: """Since "0.3.0"."""
+.} =
+  () => f().into(g)
 
 
 
-func chain*[A; B](f: () -> A; g: A -> B): () -> B =
-  () => f().g()
+func compose*[A; B; C](self: B -> C; prev: A -> B): A -> C =
+  ##[
+    Since `0.3.0`.
+  ]##
+  prev.chain(self)
