0013c90f2a6bced5821bdfa500e04c133cb51a5e
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-09 09:37:50 +0100

    Deprecate "lambda" module. (#64)

--- src/funcynim/lambda.nim
+++ src/funcynim/lambda.nim
@@ -2,6 +2,10 @@ import std/[sugar]
 
 
 
+{.deprecated: """Since "0.3.0".""".}
+
+
+
 template lambda*[T](expr: T): auto =
   ##[
     It is the same as ``() => expr``, but the method or classic call syntaxes
