32ee2490b8c37e329197c73369a5310ced26a370
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-17 11:48:59 +0100

    Add test for identity function's idempotence (#68)

--- tests/itself/test.nim
+++ tests/itself/test.nim
@@ -1,13 +1,35 @@
 when isMainModule:
-  import pkg/funcynim/[itself]
+  import pkg/funcynim/[itself, run]
 
-  import std/[unittest]
+  import std/[sugar, unittest]
 
 
 
   proc main() =
     suite "itself":
-      discard
+      test "The identity function should be idempotent.":
+        proc repeat[T](self: T -> T; n: Positive): T -> T =
+          (proc (input: T): T =
+            result = input
+
+            for _ in 0 ..< n:
+              result = self(result)
+          )
+
+
+        proc doTest[T](input: T; n: Positive) =
+          let
+            actual = itself.itself[T].repeat(n).run(input)
+            expected = input.itself()
+
+          check:
+            actual == expected
+
+
+        doTest("a", 1)
+        doTest('a', 5)
+        doTest(-1, 15)
+        doTest(FloatingPointDefect.newException(""), 3)
 
 
 
