02906fdec3037ac91992fb5e7c9109e4c957d55f
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-20 17:10:32 +0100

    chain: Add compile time test

--- tests/chain/test.nim
+++ tests/chain/test.nim
@@ -15,14 +15,36 @@ when isMainModule:
           input: A
         ) =
           let
-            leftHand = first.chain(second.chain(third))
-            rightHand = first.chain(second).chain(third)
+            leftHand = first.chain(second.chain(third))(input)
+            rightHand = first.chain(second).chain(third)(input)
 
           check:
-            leftHand(input) == rightHand(input)
+            leftHand == rightHand
 
 
         doTest((i: int) => i + 1, i => $i, s => s.len(), 0)
+
+
+
+      test "(Compile time) Chaining procs should be associative.":
+        when defined(js):
+          skip() # https://github.com/nim-lang/Nim/issues/12492
+        else:
+          proc doTest[A; B; C; D](
+            first: static[proc (_: A): B {.nimcall, noSideEffect.}];
+            second: static[proc (_: B): C {.nimcall, noSideEffect.}];
+            third: static[proc (_: C): D {.nimcall, noSideEffect.}];
+            input: static A
+          ) =
+            const
+              leftHand = first.chain(second.chain(third))(input)
+              rightHand = first.chain(second).chain(third)(input)
+
+            check:
+              leftHand == rightHand
+
+
+          doTest((i: int) => i + 1, (i: int) => $i, (s: string) => s.len(), 0)
 
 
 
