c1cb1c6f843b4a6f66e4a52eccd86579e3500e1a
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-20 17:10:41 +0100

    itself: Add compile time test

--- tests/itself/test.nim
+++ tests/itself/test.nim
@@ -5,18 +5,19 @@ when isMainModule:
 
 
 
+  proc repeat[T](self: T -> T; n: Positive): T -> T =
+    (proc (input: T): T =
+      result = input
+
+      for _ in 0 ..< n:
+        result = self(result)
+    )
+
+
+
   proc main() =
     suite "itself":
       test "The identity function should be idempotent.":
-        proc repeat[T](self: T -> T; n: Positive): T -> T =
-          (proc (input: T): T =
-            result = input
-
-            for _ in 0 ..< n:
-              result = self(result)
-          )
-
-
         proc doTest[T](input: T; n: Positive) =
           let
             actual = itself.itself[T].repeat(n).run(input)
@@ -30,6 +31,25 @@ when isMainModule:
         doTest('a', 5)
         doTest(-1, 15)
         doTest(FloatingPointDefect.newException(""), 3)
+
+
+
+    test "(Compile time) The identity function should be idempotent.":
+      when defined(js):
+        skip() # https://github.com/nim-lang/Nim/issues/12492
+      else:
+        proc doTest[T](input: static[T]; n: static Positive) =
+          const
+            actual = itself.itself[T].repeat(n).run(input)
+            expected = input.itself()
+
+          check:
+            actual == expected
+
+
+        doTest("a", 1)
+        doTest('a', 3)
+        doTest(-1, 11)
 
 
 
