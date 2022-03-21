814f83abae8562f3caaa58a8fea2c1bd1e08b292
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-21 20:33:45 +0100

    Add tests similar to lens laws

--- tests/variables/test.nim
+++ tests/variables/test.nim
@@ -8,8 +8,28 @@ when isMainModule:
   proc main() =
     suite "variables":
       test [
-        """Reading a "var" after it being written should return the written""",
-        "value."
+        """Reading a "var", then writing the read value should be the same""",
+        """as leaving the "var" unmodified."""
+      ].join($' '):
+        proc doTest[T](value: T) =
+          var sut = value
+
+          let
+            actual = sut.write(sut.read())
+            expected = value
+
+          check:
+            actual == expected
+
+
+        doTest(-5)
+        doTest("abc abc")
+        doTest(false)
+
+
+
+      test [
+        """Writing a "var", then reading it should return the written value."""
       ].join($' '):
         proc doTest[T](value: T) =
           var sut {.noInit.}: T
@@ -25,6 +45,32 @@ when isMainModule:
         doTest(2)
         doTest("abc")
         doTest(("a", '0'))
+
+
+
+      test [
+        """Writing 2 values successively in a "var" should be the same as""",
+        "writing the 2nd value directly."
+      ].join($' '):
+        proc doTest[T](first, second: T) =
+          var
+            writtenTwice {.noInit.}: T
+            writtenOnce {.noInit.}: T
+
+          require:
+            first != second
+
+          let
+            actual = writtenTwice.write(first).write(second).read()
+            expected = writtenOnce.write(second).read()
+
+          check:
+            actual == expected
+
+
+        doTest(0, int.high())
+        doTest("a", "")
+        doTest(('a', 8i16), (' ', 4i16))
 
 
 
