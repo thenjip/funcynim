1989d202b663fd80cfb99e2682ffd9ab03c9d79d
thenjip <39303840+thenjip@users.noreply.github.com>
2022-03-22 10:36:06 +0100

    `convert`: Add compile test for `to()`

--- tests/convert/test.nim
+++ tests/convert/test.nim
@@ -7,7 +7,14 @@ when isMainModule:
 
   proc main() =
     suite "convert":
-      discard
+      test """"self.to(B)" should compile.""":
+        proc doTest[A](self: A; B: typedesc) =
+          discard self.to(B)
+
+
+        doTest(0, float)
+        doTest('a', byte)
+        doTest(ValueError.newException(""), ref Exception)
 
 
 
