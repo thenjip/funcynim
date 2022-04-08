dcc88e1767063dee0fe62d62c7a69a9239367b9d
thenjip <39303840+thenjip@users.noreply.github.com>
2022-04-08 15:37:46 +0200

    Release version `1.0.0` (#84)
    * Remove deprecated modules from the docs
    
    * Remove deprecated modules from the project
    
    * Remove deprecated symbols
    
    * Bump Nimble package version to `1.0.0`
    
    * Add changelog for version `1.0.0`

--- src/funcynim/chain.nim
+++ src/funcynim/chain.nim
@@ -14,12 +14,6 @@ func chain*[A; B; C](self: A -> B; next: B -> C): A -> C =
   (a: A) => self(a).into(next)
 
 
-func chain*[A; B](f: () -> A; g: A -> B): () -> B {.
-  deprecated: """Since "0.3.0"."""
-.} =
-  () => f().into(g)
-
-
 
 func compose*[A; B; C](self: B -> C; prev: A -> B): A -> C =
   ##[
