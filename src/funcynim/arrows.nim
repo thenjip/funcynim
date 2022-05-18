2ff94ed61dc613a60e4d5ee701b95a408da6fbd4
thenjip <39303840+thenjip@users.noreply.github.com>
2022-05-18 11:35:22 +0200

    Add doc comments to `arrows`

--- src/funcynim/arrows.nim
+++ src/funcynim/arrows.nim
@@ -1,3 +1,15 @@
+##[
+  This module provides sugar for writing `Unit -> T` procedures.
+
+  Since ``1.1.0``.
+]##
+
+#[
+  TODO: Add support for pragmas.
+]#
+
+
+
 import unit
 
 import std/[sugar]
@@ -9,9 +21,23 @@ export `->`, `=>`
 
 
 template `->`*(_: Unit; R: typedesc): typedesc[proc] =
+  ##[
+    Allows to write `() -> T` instead of `Unit -> T`.
+
+    Pragmas are not supported.
+
+    Since ``1.1.0``.
+  ]##
   Unit -> R
 
 
 
 template `=>`*[R](u: Unit; body: R): proc =
+  ##[
+    Allows to write `() => body` instead of `(_: Unit) => body`.
+
+    Pragmas are not supported.
+
+    Since ``1.1.0``.
+  ]##
   (_: Unit) => body
